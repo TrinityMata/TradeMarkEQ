6f8baf6822e3403a4c297fae42eed1d8e1bcc64a
TrinityMata <trinity.m.mata@gmail.com>
2024-04-15 01:55:17 -0500

    Reverted code to before bypass buttons to fix bug

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -66,46 +66,46 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g,
     }
 }
 
-void LookAndFeel::drawToggleButton(juce::Graphics& g,
-    juce::ToggleButton& toggleButton,
-    bool shouldDrawButtonAsHighlighted,
-    bool shouldDrawButtonAsDown)
-{
-    using namespace juce;
-
-    Path powerButton;
-
-    auto bounds = toggleButton.getLocalBounds();
-
-    g.setColour(Colours::red);
-    g.drawRect(bounds);
-    auto size = jmin(bounds.getWidth(), bounds.getHeight()) - 6;
-    auto r = bounds.withSizeKeepingCentre(size, size).toFloat();
-
-    float ang = 30.f;
-
-    size -= 6;
-
-    powerButton.addCentredArc(r.getCentreX(),
-        r.getCentreY(),
-        size * 0.5,
-        size * 0.5,
-        0.f,
-        degreesToRadians(ang),
-        degreesToRadians(360.f - ang),
-        true);
-
-    powerButton.startNewSubPath(r.getCentreX(), r.getY());
-    powerButton.lineTo(r.getCentre());
-
-    PathStrokeType pst(2.f, PathStrokeType::JointStyle::curved);
-
-    auto color = toggleButton.getToggleState() ? Colours::dimgrey : Colour(0u, 172u, 1u);
-
-    g.setColour(color);
-    g.strokePath(powerButton, pst);
-    g.drawEllipse(r, 2);
-}
+//void LookAndFeel::drawToggleButton(juce::Graphics& g,
+//    juce::ToggleButton& toggleButton,
+//    bool shouldDrawButtonAsHighlighted,
+//    bool shouldDrawButtonAsDown)
+//{
+//    using namespace juce;
+//
+//    Path powerButton;
+//
+//    auto bounds = toggleButton.getLocalBounds();
+//
+//    g.setColour(Colours::red);
+//    g.drawRect(bounds);
+//    auto size = jmin(bounds.getWidth(), bounds.getHeight()) - 6;
+//    auto r = bounds.withSizeKeepingCentre(size, size).toFloat();
+//
+//    float ang = 30.f;
+//
+//    size -= 6;
+//
+//    powerButton.addCentredArc(r.getCentreX(),
+//        r.getCentreY(),
+//        size * 0.5,
+//        size * 0.5,
+//        0.f,
+//        degreesToRadians(ang),
+//        degreesToRadians(360.f - ang),
+//        true);
+//
+//    powerButton.startNewSubPath(r.getCentreX(), r.getY());
+//    powerButton.lineTo(r.getCentre());
+//
+//    PathStrokeType pst(2.f, PathStrokeType::JointStyle::curved);
+//
+//    auto color = toggleButton.getToggleState() ? Colours::dimgrey : Colour(0u, 172u, 1u);
+//
+//    g.setColour(color);
+//    g.strokePath(powerButton, pst);
+//    g.drawEllipse(r, 2);
+//}
 
 //===============================================================================
 void RotarySliderWithLabels::paint(juce::Graphics& g)
@@ -672,13 +672,13 @@ TradeMarkEQAudioProcessorEditor::TradeMarkEQAudioProcessorEditor(TradeMarkEQAudi
         addAndMakeVisible(comp);
     }
 
-    lowpeakBypassButton.setLookAndFeel(&lnf);
-    midlowpeakBypassButton.setLookAndFeel(&lnf);
-    midpeakBypassButton.setLookAndFeel(&lnf);
-    midhighpeakBypassButton.setLookAndFeel(&lnf);
-    highpeakBypassButton.setLookAndFeel(&lnf);
-    lowcutBypassButton.setLookAndFeel(&lnf);
-    highcutBypassButton.setLookAndFeel(&lnf);
+    //lowpeakBypassButton.setLookAndFeel(&lnf);
+    //midlowpeakBypassButton.setLookAndFeel(&lnf);
+    //midpeakBypassButton.setLookAndFeel(&lnf);
+    //midhighpeakBypassButton.setLookAndFeel(&lnf);
+    //highpeakBypassButton.setLookAndFeel(&lnf);
+    //lowcutBypassButton.setLookAndFeel(&lnf);
+    //highcutBypassButton.setLookAndFeel(&lnf);
 
 
     setSize(600, 500);
